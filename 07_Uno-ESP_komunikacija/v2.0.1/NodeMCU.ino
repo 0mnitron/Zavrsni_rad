1aecfa408897daafe66acc371610386c0021f488
0mnitron <56760837+0mnitron@users.noreply.github.com>
2021-03-07 00:06:40 +0100

    Update NodeMCU.ino

--- 07_Uno-ESP_komunikacija/v2.0.1/NodeMCU.ino
+++ 07_Uno-ESP_komunikacija/v2.0.1/NodeMCU.ino
@@ -8,15 +8,20 @@ SoftwareSerial Uno(4,5);  //Rx; Tx
 void setup() {
   Serial.begin(115200);
   while (!Serial) continue;
-  
+
+  // komunikacija s Arduino Uno
   Uno.begin(115200);
 }
 
 void loop() {
+  // ako je Arduino Uno poslao neke podatke
   if(Uno.available()){
+    // osigurana veličina prostora za podatke je 300 byta  -> prilagoditi poslije
     StaticJsonDocument<300> doc;
+    // provjeriti da li je deserijalizacija uspješno obavljena
     DeserializationError err = deserializeJson(doc, Uno);  
-    
+
+    // ako je uspješno obavljena:
     if(err == DeserializationError::Ok){
       float t = doc["temperature"];
       float h = doc["humidity"];
@@ -27,14 +32,17 @@ void loop() {
       Serial.print(h);
       Serial.println("\n----------------------------");
     }
+    // ako nije:
     else{
       // Print error to the "debug" serial port
       Serial.print("deserializeJson() returned ");
       Serial.println(err.c_str());
 
-      // Flush all bytes in the "link" serial port buffer
-      while (Uno.available() > 0)
+      // Flush all bytes in the "Uno" serial port buffer
+      // zapravo ovaj dio koda isprazni buffer tako što pročita podatke, pa novi podatci mogu doći
+      while (Uno.available() > 0){
         Uno.read();
+      }
     }
   }
 }
